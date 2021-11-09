7823af4636f12d998bebc8ad1ae7ce4deb39806d
Reshma Goolamy <com.goolamy@gmail.com>
2021-11-09 18:01:49 +0100

    Camera commit

---
+++ Assets/mouseLook.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y")
+    }
+}
